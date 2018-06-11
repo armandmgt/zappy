fee32adbc6cb065b673c0a7014036f5e8403dab3
armandmgt <armand.megrot@epitech.eu>
2018-06-11 22:58:45 +0200

    initial files and CMakeLists

---
+++ sources/server/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2017
+** zappy
+** File description:
+** Created by armandmgt,
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	fprintf(stdout, "hello world from server\n");
+	return (0);
+}
