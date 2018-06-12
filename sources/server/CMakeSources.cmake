8542e77220435eb74384399d979a436505e48c62
armandmgt <armand.megrot@epitech.eu>
2018-06-12 22:29:13 +0200

    remove useless dependency on include files

--- sources/server/CMakeSources.cmake
+++ sources/server/CMakeSources.cmake
@@ -2,8 +2,6 @@ set(CORE
 	main.c
     options_parsing/parse_options.c
     options_parsing/parse_functions.c
-    ../../include/server/server.h
-    ../../include/server/parse_functions.h
 )
 
 set(SRCS
