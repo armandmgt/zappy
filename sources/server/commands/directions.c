2f305a7749c23d4476ea41d01c04ad94475288dd
marianne.levee <marianne.levee@epitech.eu>
2018-06-14 18:16:04 +0200

    A beginning of inhabitants' actions (commands) commands.h / actions / directions / states

---
+++ sources/server/commands/directions.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** command_directions
+** File description:
+** none
+*/
+
+#include "server/server.h"
+
+char *forward(cell_t *cells)
+{
+	return ("ok\n");
+}
+
+char *right(cell_t *cells)
+{
+	return ("ok\n");
+}
+
+char *left(cell_t *cells)
+{
+	return ("ok\n");
+}
+
+char *look(void)
+{
+	return ("tile1 [...]\n");
+}
