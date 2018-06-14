c9d6d2af97f9082376c717a1420b3ced0bd4f2b5
marianne.levee <marianne.levee@epitech.eu>
2018-06-14 20:34:10 +0200

    It's not working, it's just a progression of my idea commands.h: add proto for level functions actions.c: take object ok, but maybe change statesc.: inventory + incantation, but maybe big change too level.c/level2.c: idea of how to implement dat'

--- include/server/commands.h
+++ include/server/commands.h
@@ -7,11 +7,20 @@
 
 #pragma once
 
+#include <stddef.h>
+
 typedef struct command_s
 {
 	char *cmd;
 	char *(*ptr_func)(void);
 } command_t;
+
+typedef struct infos_s
+{
+	size_t level;
+	size_t *ressources;
+	int nb_player;
+} infos_t;
 
 char *forward(cell_t *cells);
 char *right(cell_t *cells);
@@ -26,3 +35,11 @@ char *take(cell_t *cells);
 char *set(cell_t *cells);
 char *incantation(player_t *player);
 char *death(void);
+
+void level2(infos_t *infos);
+void level3(infos_t *infos);
+void level4(infos_t *infos);
+void level5(infos_t *infos);
+void level6(infos_t *infos);
+void level7(infos_t *infos);
+void level8(infos_t *infos);
