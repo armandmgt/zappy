087a66f5e4ec42a52740e007ea9228066f64ad25
Tom <tom.roch4t@gmail.com>
2018-06-14 15:05:52 +0200

    Reorganize files and add func to parse tile
    > Move type Direction + directions const to common.go
    > Add type Inventory with 7 slots, one for each ressource
    > Add Inventory to Client struct
    > Add generic getResposneData to avoid slicing manually
    > gameLoop now takes a Client pointer
    > Add function to initClient to avoid having to change the litteral
    initialisation everytime we change the Client struct
    > Debug will now redefine the team name
    > Format for that ugly fucking map of functions
    > add getTileContent, which answers the bct command

--- sources/client/common.go
+++ sources/client/common.go
@@ -1,6 +1,23 @@
 package main
 
+import `strings`
+
 type Map struct {
 	X int64 `json:"x"`
 	Y int64 `json:"y"`
+}
+
+type Direction int8
+const (
+	N 	Direction = 1
+	E	Direction = 2
+	S	Direction = 3
+	W	Direction = 4
+)
+
+type Inventory [7]int64
+
+func getResponseData(s string) []string {
+	// Since all server responses start with xxx, we always cut at the 4th index
+	return strings.Split(s[4:], " ")
 }
