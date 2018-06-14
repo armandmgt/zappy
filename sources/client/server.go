7651bd527ebc54c11633943f1db698837d470944
Tom <tom.roch4t@gmail.com>
2018-06-14 10:32:58 +0200

    Start debug mode for client
    > Clean up redundant fatal log
    > Add debug flag
    > Rename ai to c for Client
    > Add debug loop. For now, it reads stdin until EOF and prints what it
    read
    > Implements getMapSize function

--- sources/client/server.go
+++ sources/client/server.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	`strings`
+	`strconv`
+	`log`
+)
+
 var Responses = map[string]func(c *Client, s string) {
 	"msz": getMapSize,
 	"bct": nil,
@@ -29,4 +35,15 @@ var Responses = map[string]func(c *Client, s string) {
 }
 
 func getMapSize(c *Client, s string) {
+	arr := []int64{-1, -1}
+	data := strings.Split(s[4:], " ")
+	for i, d := range data {
+		if v, e := strconv.Atoi(d); e != nil {
+			log.Println("[msz]\tFailed to parse server response")
+		} else {
+			arr[i] = int64(v)
+		}
+	}
+	c.gameMap.X = arr[0]
+	c.gameMap.Y = arr[1]
 }
