85dca785d55b03888e70fc18c54c18708e392b89
Tom <tom.roch4t@gmail.com>
2018-06-15 11:56:25 +0200

    Refactor a bit of protocol code
    > Add a function which will return the player number with the data since
    most of the protocol responses includes it.
    THIS IS BOUND TO CHANGE for a better user handling (use the player
    number to get the correct player and change his data directly)
    
    > data is now a global array to avoid recreating a new array on every
    protocol response
    > Change protocol functions to reflect this change

--- sources/client/common.go
+++ sources/client/common.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	`strings`
+	`strconv`
 )
 
 type Map struct {
@@ -26,10 +27,23 @@ const (
 
 type Inventory [7]int64
 
-func getProtocolResponseData(s string) []string {
+func getProtocolResponseData(s string) (a []string) {
 	// Since all server responses start with xxx, we always cut at the 4th index
 	if len(s) == 3 {
-		return []string{}
+		return a
 	}
 	return strings.Split(s[4:], " ")
+}
+
+func getProtocolResponseDataWithPlayerNumber(s string) (a []string, _ int64) {
+	// Since all server responses start with xxx, we always cut at the 4th index
+	if len(s) == 3 {
+		return a, 0
+	}
+	a = strings.Split(s[4:], " ")
+	t, e := strconv.Atoi(a[0])
+	if e != nil {
+		return a, 0
+	}
+	return a, int64(t)
 }
