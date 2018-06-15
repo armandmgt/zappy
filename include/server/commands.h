75e014292c9fd3682c72f68bbfe1af0d840c035a
marianne.levee <marianne.levee@epitech.eu>
2018-06-15 11:47:10 +0200

    New changes - level.c / level2.c = replace by just 1 function commands.h: suppr tab struct infos (unused) + add maccro to know the len of command_t  + change proto of ptr_func actions/directions/states: idea of how to implement forward, right, left, inventory and incantation

--- include/server/commands.h
+++ include/server/commands.h
@@ -9,37 +9,24 @@
 
 #include <stddef.h>
 
+static const int CMD_LEN = 13;
+
 typedef struct command_s
 {
 	char *cmd;
-	char *(*ptr_func)(void);
+	char *(*ptr_func)(cell_t *cells);
 } command_t;
-
-typedef struct infos_s
-{
-	size_t level;
-	size_t *ressources;
-	int nb_player;
-} infos_t;
 
 char *forward(cell_t *cells);
 char *right(cell_t *cells);
 char *left(cell_t *cells);
-char *look(void);
-char *inventory(player_t *player);
-char *broadcast(void);
-char *connect(void);
-char *born(void);
-char *eject(void);
+char *look(cell_t *cells);
+char *inventory(cell_t *cells);
+char *broadcast(cell_t *cells);
+char *connect(cell_t *cells);
+char *born(cell_t *cells);
+char *eject(cell_t *cells);
 char *take(cell_t *cells);
 char *set(cell_t *cells);
-char *incantation(player_t *player);
-char *death(void);
-
-void level2(infos_t *infos);
-void level3(infos_t *infos);
-void level4(infos_t *infos);
-void level5(infos_t *infos);
-void level6(infos_t *infos);
-void level7(infos_t *infos);
-void level8(infos_t *infos);
+char *incantation(cell_t *cells);
+char *death(cell_t *cells);
--- sources/server/CMakeSources.cmake
+++ sources/server/CMakeSources.cmake
@@ -9,6 +9,10 @@ set(CORE
     connexion/new_connection.c
 
     ../common/linked_list.c
+
+    commands/actions.c
+    commands/directions.c
+    commands/states.c
 )
 
 set(SRCS
