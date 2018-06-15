5895a76625bc647f2f0b0912cc9b050d20e88e15
marianne.levee <marianne.levee@epitech.eu>
2018-06-15 15:18:59 +0200

    In progress commands.h: struct cell_t unused but I need it actions.c: beginning of birth + eject states.c: beginning of death

--- sources/server/commands/actions.c
+++ sources/server/commands/actions.c
@@ -22,13 +22,40 @@ char *connect(cell_t *cells)
 	return ("value\n");
 }
 
-char *born(cell_t *cells)
+char *birth(cell_t *cells)
 {
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	else if (pid == 0) {
+		/* créer un oeuf et attendre qu'un client puisse se co*/
+	}
 	return ("ok\n");
 }
 
 char *eject(cell_t *cells)
 {
+	switch (cells->player->direction)
+	{
+		case (cells->player->direction == 1):
+		cells->player->pos[y][x] = cells->player->pos[y + 1][x];
+		return ("ok\n");
+		//return ("eject: S\n"); /* si y a des gens sur la case */
+		case (cells->player->direction == 2):
+		cells->player->pos[y][x] = cells->player->pos[y][x + 1];
+		return ("ok");
+		//return ("eject: W\n"); /* si y a des gens sur la case */
+		case (cells->player->direction == 3):
+		cells->player->pos[y][x] = cells->player->pos[y - 1][x];
+		return ("ok");
+		//return ("eject: N\n"); /* si y a des gens sur la case */
+		case (cells->player->direction == 4):
+		cells->player->pos[y][x] = cells->player->pos[y][x - 1];
+		return ("ok");
+		//return ("eject: E\n"); /* si y a des gens sur la case */
+	}
 	return ("ko\n");
 }
 
@@ -37,7 +64,7 @@ char *take(cell_t *cells)
 	if (cells[y][x] && cells[y][x]->ressources) {
 		while (cells[y][x]->ressources != NULL) {
 			cells[y][x]->player->inventory += cells[y][x]->ressources;
-			cells[y][x]->ressources -= cells[y][x]->ressources;
+			cells[y][x]->ressources -= 1;
 		}
 		return ("ok\n");
 	}
@@ -46,5 +73,12 @@ char *take(cell_t *cells)
 
 char *set(cell_t *cells)
 {
+	if (cells[y][x] && cells->player->inventory) {
+		while (cells->player->inventory != NULL) {
+			cells[y][x]->player->inventory -= cells[y][x]->ressources;
+			cells[y][x]->ressources += 1;
+		}
+		return ("ok\n");
+	}
 	return ("ko\n");
 }
