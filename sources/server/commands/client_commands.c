3bb36aff67cf99250663bdfb2c41fe143c93ad18
armandmgt <armand.megrot@epitech.eu>
2018-06-18 19:31:57 +0200

    change struct datas

---
+++ sources/server/commands/client_commands.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2017
+** Project
+** File description:
+** Created by armandmgt,
+*/
+
+#include "server.h"
+
+int poll_client_commands(server_t *server, fd_set *readfds)
+{
+	return (0);
+}
+
+int do_pending_actions(server_t *server)
+{
+	return (0);
+}
