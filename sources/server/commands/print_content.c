650293e1ba56261b92f62ded73d957050e231b3d
Thomas <thomas.derote-parcellier@epitech.eu>
2018-06-20 21:33:05 +0200

    Add - NB_DIRECTION to direction_t enum Add - add random direction at client spawn Fix - fix of vision range related to level Fix - fix of vision rotation -> always looking from left to right and from inside to outside (relative to direction) Add - default case to switch (needed since NB_DIRECTION was added)

--- sources/server/commands/print_content.c
+++ sources/server/commands/print_content.c
@@ -7,7 +7,6 @@
 
 #include <stdio.h>
 #include "server.h"
-#include "resources.h"
 
 static void print_cell_content(cell_t *cell, client_t *client);
 static uint32_t wrapped_coord(int32_t coord, uint32_t range);
@@ -16,7 +15,8 @@ void print_line_content(map_t *map_infos, client_t *client,
 	int32_t tmp_y, uint32_t tiles)
 {
 	cell_t *tmp_cell;
-	int32_t tmp_x = client->infos->pos.x  - (tiles / 2);
+	int32_t tmp_x = client->infos->direction == NORTH ? client->infos->pos
+		.x  - (tiles /	2) : client->infos->pos.x  + (tiles /	2);
 	uint32_t x = wrapped_coord(tmp_x, map_infos->x);
 	uint32_t y = wrapped_coord(tmp_y, map_infos->y);
 
@@ -26,15 +26,18 @@ void print_line_content(map_t *map_infos, client_t *client,
 		x = wrapped_coord(x, map_infos->x);
 		tmp_cell = get_cell_at(map_infos, x, y);
 		print_cell_content(tmp_cell, client);
-		x++;
+		x = client->infos->direction == NORTH ? x + 1 : x - 1;
 	}
+	if (tiles == 1)
+		dprintf(client->sock, ",");
 }
 
 void print_row_content(map_t *map_infos, client_t *client,
 	int32_t tmp_x, uint32_t tiles)
 {
 	cell_t *tmp_cell;
-	int32_t tmp_y = client->infos->pos.y  - (tiles / 2);
+	int32_t tmp_y = client->infos->direction == EAST ? client->infos->pos
+		.y  - (tiles /	2) : client->infos->pos.y  + (tiles /	2);
 	uint32_t y = wrapped_coord(tmp_y, map_infos->y);
 	uint32_t x = wrapped_coord(tmp_x, map_infos->x);
 
@@ -44,8 +47,10 @@ void print_row_content(map_t *map_infos, client_t *client,
 		y = wrapped_coord(y, map_infos->y);
 		tmp_cell = get_cell_at(map_infos, x, y);
 		print_cell_content(tmp_cell, client);
-		y++;
+		y = client->infos->direction == EAST ? y + 1 : y - 1;
 	}
+	if (tiles == 1)
+		dprintf(client->sock, ",");
 }
 
 static void print_cell_content(cell_t *cell, client_t *client)
@@ -58,17 +63,12 @@ static void print_cell_content(cell_t *cell, client_t *client)
 	};
 	size_t len = list_len(cell->players);
 
-	for (size_t i = 0; i < len; i++) {
-		if (i > 0)
-			dprintf(client->sock, " ");
-		dprintf(client->sock, "player");
-	}
+	for (size_t i = 0; i < len; i++)
+		dprintf(client->sock, " player");
 	for (size_t i = 0; i < NB_RESOURCE; i++) {
-		if (i != 0 || len)
-			dprintf(client->sock, " ");
 		for (int j = 0; j < get_resource_on_cell(cell,
 			(resource_t)i); j++)
-			dprintf(client->sock, "%s", res_name[i].name);
+			dprintf(client->sock, " %s", tab[i].name);
 	}
 }
 
