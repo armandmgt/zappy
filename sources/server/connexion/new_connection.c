eed6c0682564d9efd8be2320327dac5afc4c3a90
Thomas <thomas.derote-parcellier@epitech.eu>
2018-06-20 18:31:02 +0200

    Fix = just a coding style fix

--- sources/server/connexion/new_connection.c
+++ sources/server/connexion/new_connection.c
@@ -60,7 +60,8 @@ static void spawn_client_on_map(map_t *m, client_t *client)
 	client->infos->pos.x = (uint32_t)rand() % m->x;
 	client->infos->pos.y = (uint32_t)rand() % m->y;
 	//TODO: add random direction when PR #35 is merged in
-	players = get_player_list_at(m, client->infos->pos.x, client->infos->pos.y);
+	players = get_player_list_at(m, client->infos->pos.x,
+		client->infos->pos.y);
 	add_elem_at_front(players, client);
 }
 
