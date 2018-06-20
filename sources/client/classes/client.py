99a7bff7de04fbd76c0d6c440549dea85f4aea94
Tom <tom.roch4t@gmail.com>
2018-06-21 01:22:10 +0200

    Sailing away from Go to Python
    > I guess snakes are better suited for this project than gophers
    > Whatever
    
    > Add main.py which parses the args
    > Add client class with a socket and connection informations

---
+++ sources/client/classes/client.py
@@ -0,0 +1,33 @@
+import socket
+
+_max_buffer_size = 4096
+
+
+class Client:
+	sock = socket.socket()
+	port = -1
+	teamName = ""
+	host = ""
+
+	def __init__(self, port, name, host):
+		self.host = host
+		self.port = port
+		self.teamName = name
+
+	def connect(self):
+		self.sock.connect((self.host, self.port))
+
+	def read(self):
+		return self.sock.recv(_max_buffer_size)
+
+	def write(self, data):
+		self.sock.send(data)
+
+	def terminate(self):
+		self.sock.close()
+
+# 	debug methods
+	def dump(self):
+		print('port:\t', self.port)
+		print('team:\t', self.teamName)
+		print('host:\t', self.host)
