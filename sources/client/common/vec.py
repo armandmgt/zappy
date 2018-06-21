35a97284fc890a294ef2027aad82dd18486023a9
Tom <tom.roch4t@gmail.com>
2018-06-21 12:25:16 +0200

    Add several method to client and better python code
    > Add player to client
    > single quotes for the sake of C O N S I S T E N C Y
    > Add move_forward, turn_right, turn_left methods
    > Replace type check with isinstance(...)

--- sources/client/common/vec.py
+++ sources/client/common/vec.py
@@ -3,7 +3,7 @@ class Vec2d:
 	__y = None
 
 	def __init__(self, x, y):
-		if type(x) is not type(y):
+		if type(x) != type(y):
 			raise RuntimeError("A vector must have both elements of the same type")
 		self.__x = x
 		self.__y = y
@@ -13,3 +13,13 @@ class Vec2d:
 
 	def second(self):
 		return self.__y
+
+	def set_x(self, val):
+		if not isinstance(val, type(self.__x)):
+			return
+		self.__x = val
+
+	def set_y(self, val):
+		if not isinstance(val, type(self.__y)):
+			return
+		self.__y = val
