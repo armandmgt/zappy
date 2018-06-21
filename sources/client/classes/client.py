17c26c0fd18aa27376506c005e1c551e24ea3808
Tom <tom.roch4t@gmail.com>
2018-06-21 12:25:16 +0200

    Add several method to client and better python code
    > Add player to client
    > single quotes for the sake of C O N S I S T E N C Y
    > Add move_forward, turn_right, turn_left methods
    > Replace type check with isinstance(...)

--- sources/client/classes/client.py
+++ sources/client/classes/client.py
@@ -1,14 +1,15 @@
 import socket
 from common.vec import Vec2d
+from classes.player import Player
 
 _max_buffer_size = 4096
 
 
 class Client:
-	player = None
+	player: Player = None
 	sock = socket.socket()
 	port = -1
-	team = ""
+	team = ''
 	mapSize = Vec2d(0, 0)
 	host: str
 	slotsLeft: int
@@ -18,6 +19,8 @@ class Client:
 		self.port = port
 		self.team = name
 
+		self.player = Player(pid=0, pos=Vec2d(0, 0))
+
 	def connect(self):
 		self.sock.connect((self.host, self.port))
 
@@ -25,14 +28,47 @@ class Client:
 		return self.sock.recv(_max_buffer_size).decode()
 
 	def write(self, data):
-		data += '\n'
+		if not data.endswith('\n'):
+			data += '\n'
 		self.sock.send(data.encode())
 
 	def terminate(self):
 		self.sock.close()
 
 	def get_initial_data(self, data: str):
-		datalist = data.split("\n")
+		datalist = data.split('\n')
 		self.slotsLeft = int(datalist[0])
-		s = datalist[1].split(" ")
+		s = datalist[1].split(' ')
 		self.mapSize = Vec2d(int(s[0]), int(s[1]))
+
+	def move_forward(self):
+		self.write('Forward')
+		if self.player.orientation == 0:  # NORTH
+			self.player.position.set_x((self.player.position.first() + 1) % self.mapSize.first())
+		elif self.player.orientation == 1:  # SOUTH
+			self.player.position.set_x((self.player.position.first() - self.mapSize.first()) % self.mapSize.first())
+		elif self.player.orientation == 2:  # EAST
+			self.player.position.set_y((self.player.position.second() + 1) % self.mapSize.second())
+		elif self.player.orientation == 3:  # WEST
+			self.player.position.set_y((self.player.position.second() - self.mapSize.second()) % self.mapSize.second())
+
+	def turn_right(self):
+		self.write('Right')
+		self.player.orientation = (self.player.orientation + 1) % 4
+
+	def turn_left(self):
+		self.write('Left')
+		self.player.orientation = (self.player.orientation - 1) % 4
+
+	def look(self):
+		self.write('Look')
+		response = self.read()
+		translator = str.maketrans('', '', '[]\n')
+		data = response.translate(translator).strip().split(',')
+		i = 0
+
+		for s in data:
+			segment = s.strip().split(' ')
+			for key in segment:
+				self.player.vision[i][key] += 1
+			i += 1
--- sources/client/main.py
+++ sources/client/main.py
@@ -4,11 +4,11 @@ from classes.client import Client
 
 
 def get_params():
-	parser = argparse.ArgumentParser(add_help=False, usage="-p <port> -n <team> [-h <host>]")
-	parser.add_argument('-h', type=str, help="Host")
+	parser = argparse.ArgumentParser(add_help=False, usage='-p <port> -n <team> [-h <host>]')
+	parser.add_argument('-h', type=str, help='Host')
 	try:
-		parser.add_argument('-p', type=int, help="Port number")
-		parser.add_argument('-n', type=str, help="Team name")
+		parser.add_argument('-p', type=int, help='Port number')
+		parser.add_argument('-n', type=str, help='Team name')
 		args = parser.parse_args()
 		assert args.p > 0 and len(args.n) > 0
 	except argparse.ArgumentError and AssertionError:
@@ -19,14 +19,15 @@ def get_params():
 def run_game(c: Client):
 	c.connect()
 	welcome = c.read()
-	if welcome != "WELCOME\n":
-		raise RuntimeError("invalid first message; got:\t", welcome)
+	if welcome != 'WELCOME\n':
+		raise RuntimeError('invalid first message; got:\t', welcome)
 	c.write(c.team)
 	c.get_initial_data(c.read())
+	c.look()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	params = get_params()
 
-	client = Client(port=params.p, name=params.n, host=params.h if params.h is not None else "127.0.0.1")
+	client = Client(port=params.p, name=params.n, host=params.h if params.h is not None else '127.0.0.1')
 	run_game(client)
