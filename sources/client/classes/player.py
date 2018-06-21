574ef150ddf296a6649c0008bd5c195e3e1766de
Tom <tom.roch4t@gmail.com>
2018-06-21 09:10:08 +0200

    Minor changes
    > Update Except clauses
    > remove __pycache__, sorry :(

--- sources/client/classes/player.py
+++ sources/client/classes/player.py
@@ -5,14 +5,14 @@ class Player:
 	id = -1
 	team = None
 	level = 1
-	position = Vec2d(-1, -1)
+	position = None
 	orientation = 0
 	inventory = {"food": 0, "sibur": 0, "phiras": 0, "mendiane": 0, "thystame": 0, "linemate": 0, "deraumere": 0}
 	vision = []
-	for x in range(8):
-		vision.append({"food": 0, "sibur": 0, "phiras": 0, "mendiane": 0, "thystame": 0, "linemate": 0, "deraumere": 0})
 
-	def __init__(self, team: str, pos: Vec2d, no: int):
+	def __init__(self, team: str, no: int, pos: Vec2d or None):
 		self.team = team
 		self.id = no
-		self.position = pos
+		self.position = pos if not None else Vec2d(0, 0)
+		for x in range(8):
+			self.vision.append({"food": 0, "sibur": 0, "phiras": 0, "mendiane": 0, "thystame": 0, "linemate": 0, "deraumere": 0})
--- sources/client/main.py
+++ sources/client/main.py
@@ -1,8 +1,9 @@
 import argparse
 import sys
 
-from classes import client
-# noinspection PyBroadException
+from classes.client import Client
+from classes.player import Player
+from common.vec import Vec2d
 
 
 def get_params():
@@ -13,11 +14,14 @@ def get_params():
 		parser.add_argument('-n', type=str, help="Team name")
 		args = parser.parse_args()
 		assert args.p > 0 and len(args.n) > 0
-	except Exception:
+	except argparse.ArgumentError and AssertionError:
 		sys.exit(1)
 	return args
 
 
 if __name__ == "__main__":
 	params = get_params()
-	c = client.Client(port=params.p, name=params.n, host=params.h if params.h is not None else "127.0.0.1")
+
+	c = Client(port=params.p, name=params.n, host=params.h if params.h is not None else "127.0.0.1")
+	v = Vec2d(0, 0)
+	p = Player(team=c.teamName, no=0, pos=None)
