b5dae4df8877482eec2e8471e9dcafaf013d3ace
Tom <tom.roch4t@gmail.com>
2018-06-21 10:26:34 +0200

    Add main function (run_game). For now it only get the initial information, no gameloop is present and no more informations is sent to the server

--- sources/client/classes/client.py
+++ sources/client/classes/client.py
@@ -1,33 +1,38 @@
 import socket
+from common.vec import Vec2d
 
 _max_buffer_size = 4096
 
 
 class Client:
+	player = None
 	sock = socket.socket()
 	port = -1
-	teamName = ""
-	host = ""
+	team = ""
+	mapSize = Vec2d(0, 0)
+	host: str
+	slotsLeft: int
 
 	def __init__(self, port: int, name: str, host: str):
 		self.host = host
 		self.port = port
-		self.teamName = name
+		self.team = name
 
 	def connect(self):
 		self.sock.connect((self.host, self.port))
 
-	def read(self):
-		return self.sock.recv(_max_buffer_size)
+	def read(self) -> str:
+		return self.sock.recv(_max_buffer_size).decode()
 
 	def write(self, data):
-		self.sock.send(data)
+		data += '\n'
+		self.sock.send(data.encode())
 
 	def terminate(self):
 		self.sock.close()
 
-# 	debug methods
-	def dump(self):
-		print('port:\t', self.port)
-		print('team:\t', self.teamName)
-		print('host:\t', self.host)
+	def get_initial_data(self, data: str):
+		datalist = data.split("\n")
+		self.slotsLeft = int(datalist[0])
+		s = datalist[1].split(" ")
+		self.mapSize = Vec2d(int(s[0]), int(s[1]))
