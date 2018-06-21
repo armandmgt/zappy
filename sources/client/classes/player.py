46aba1f3bd0b7ae8d9d3d657c08a5ebd139ad035
Tom <tom.roch4t@gmail.com>
2018-06-21 15:56:46 +0200

    Fix directions + add incantation

--- sources/client/classes/player.py
+++ sources/client/classes/player.py
@@ -5,6 +5,7 @@ class Player:
 	id = -1
 	level = 1
 	position: Vec2d = Vec2d(0, 0)
+	busy: bool = False
 	orientation = 0
 	inventory = dict(food=10, sibur=0, phiras=0, mendiane=0, thystame=0, linemate=0, deraumere=0, player=0)
 	vision = []
