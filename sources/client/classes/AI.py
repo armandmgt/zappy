f02a2697b1ef252f434f0436b13dc561379466c6
armandmgt <armand.megrot@epitech.eu>
2018-06-22 16:18:41 +0200

    make decision now handles only broadcast messages

--- sources/client/classes/AI.py
+++ sources/client/classes/AI.py
@@ -8,5 +8,15 @@ class AI:
 	def __init__(self, c: Client):
 		self.client = c
 
-	def make_decision(self):
+	def make_decision(self, response: str):
+		if not response:
+			self.client.send_information()
+			return
+		print(f'make decision with {response}')
+		command, *arg = response.split(maxsplit=1)
+		arg = arg[0] if arg else ''
+		print(f'command [{command}] arg [{arg}]')
+		self.parse_information()
+
+	def parse_information(self):
 		pass
