79cfaab7b0139ecd9d4976292cffcb42089b0740
marianne.levee <marianne.levee@epitech.eu>
2018-06-22 15:29:18 +0200

    Add broadcast command Need a pull request and some test

--- sources/server/commands/broadcast.c
+++ sources/server/commands/broadcast.c
@@ -13,14 +13,23 @@
 #include "gui_commands.h"
 
 static void loop_on_clients(server_t *server, client_t *client, char *args);
-static double inverse_positions(client_t *client, client_t *tmp);
-static double the_smallest(client_t *UNUSED(client), client_t *UNUSED(tmp),
-	double tan);
-static void convertion_to_position(server_t *server, client_t *client,
-	double angle);
+static int convertion_to_degree(client_t *client, double angle);
+static int get_direction(server_t *server, client_t *client, client_t *tmp);
+static int determine_angle(int angle);
 
 bool broadcast(server_t *server, client_t *client, char *args)
 {
+	client_t *tmp_client;
+
+	for (list_t *tmp = server->clients; tmp; tmp = tmp->next) {
+		tmp_client = tmp->data;
+		if (tmp_client != client && (client->infos->pos.x ==
+			tmp_client->infos->pos.x && client->infos->pos.y ==
+			tmp_client->infos->pos.y)) {
+			dprintf(tmp_client->sock, "message 0, %s", args);
+			dprintf(tmp_client->sock, "ok\n");
+		}
+	}
 	loop_on_clients(server, client, args);
 	dprintf(client->sock, "ok\n");
 	print_in_gui(server->clients, "pbc %d %s\n", client->infos->id, args);
@@ -29,44 +38,66 @@ bool broadcast(server_t *server, client_t *client, char *args)
 
 static void loop_on_clients(server_t *server, client_t *client, char *args)
 {
-	list_t *list = server->clients;
 	client_t *tmp;
+	int direction;
+
+	for (list_t *list = server->clients; list; list = list->next) {
+		tmp = list->data;
+		if (tmp != client && (client->infos->pos.x !=
+			tmp->infos->pos.x && client->infos->pos.y !=
+			tmp->infos->pos.y)) {
+			direction = get_direction(server, client, tmp);
+			dprintf(tmp->sock, "message %d, %s\n", direction, args);
+		}
+	}
+}
+
+static int get_direction(server_t *server, client_t *client, client_t *tmp)
+{
 	double delta_x;
 	double delta_y;
 	double tan;
 	double result;
+	int direction;
 
-	while (list) {
-		tmp = list->data;
-		delta_x = client->infos->pos.x - tmp->infos->pos.x;
-		delta_y = client->infos->pos.y - tmp->infos->pos.y;
-		tan = delta_y / delta_x;
-		result = the_smallest(client, tmp, tan);
-		convertion_to_position(server, client, result);
-		dprintf(tmp->sock, "message %lf, %s\n", result, args);
-		list = list->next;
-	}
+	delta_x = abs(client->infos->pos.x - tmp->infos->pos.x);
+	delta_y = abs(client->infos->pos.y - tmp->infos->pos.y);
+	if (delta_x > server->map_infos.x / 2)
+		delta_x = server->map_infos.x - delta_x;
+	if (delta_y > server->map_infos.y / 2)
+		delta_y = server->map_infos.y - delta_y;
+	tan = delta_y / delta_x;
+	result = atan(tan);
+	direction = convertion_to_degree(client, result);
+	return (direction);
 }
 
-static double the_smallest(client_t *client, client_t *tmp, double tan)
+static int convertion_to_degree(client_t *client, double angle)
 {
-	double i_tan = inverse_positions(client, tmp);
 
-	if (i_tan - tan > 0)
-		return (atan(tan));
-	return (atan(i_tan));
-}
-
-static double inverse_positions(client_t *client, client_t *tmp)
-{
-	double i_delta_x = tmp->infos->pos.x - client->infos->pos.x;
-	double i_delta_y = tmp->infos->pos.y - client->infos->pos.y;
-	double i_tan = i_delta_y / i_delta_x;
-	return (i_tan);
-}
-
-static void convertion_to_position(server_t *UNUSED(server), client_t *UNUSED(client),
-	double angle)
-{
 	double degrees = (angle * 180.0 / M_PI);
+	int orientation = determine_angle(degrees);
+	int direction = (8 - client->infos->direction + orientation) % 8;
+	return (direction);
+}
+
+static int determine_angle(int angle)
+{
+	if (angle <= 22.5 && angle >= 337.7)
+		return (7);
+	else if (angle < 67.5 && angle > 22.5)
+		return (8);
+	else if (angle >= 67.5 && angle <= 112.5)
+		return (1);
+	else if (angle > 112.5 && angle < 157.5)
+		return (2);
+	else if (angle >= 157.5 && angle < 202.5)
+		return (3);
+	else if (angle > 202.5 && angle < 247.5)
+		return (4);
+	else if (angle >= 247.5 && angle <= 292.5)
+		return (5);
+	else if (angle > 292.5 && angle < 337.5)
+		return (6);
+	return (-1);
 }
