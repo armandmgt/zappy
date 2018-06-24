251da1f784d2157cfb5cb0f5097806dafd7a1185
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-06-24 19:26:27 +0200

    fix death in whole server

--- sources/server/commands/death.c
+++ sources/server/commands/death.c
@@ -6,7 +6,9 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include "server/gui_magic.h"
 #include "common/tools.h"
 #include "server/server.h"
 #include "gui_commands.h"
@@ -21,7 +23,8 @@ static void print_ko(client_t *client, list_t *cmds)
 bool death(server_t *server, client_t *client, char *UNUSED(args))
 {
 	for (list_t *tmp = server->clients; tmp; tmp = tmp->next) {
-		if (tmp->data == client && client->infos->lifetime == 0) {
+		if (tmp->data == client && client->infos->lifetime == 0 &&
+			client->team && strcmp(client->team->name, GUI_NAME)) {
 			dprintf(client->sock, "dead\n");
 			print_in_gui(server->clients, "pdi %d\n",
 				     client->infos->id);
